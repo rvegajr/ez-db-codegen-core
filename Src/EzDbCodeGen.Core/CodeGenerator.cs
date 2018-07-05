5d30137ff5feac5b8db01f7ef44a8e077a9b21fd
Ricardo Vega <github@noctusoft.com>
2018-07-05 01:23:55 -0500

    Completed code generation tests for both multiple files and single files.

---
+++ Src/EzDbCodeGen.Core/CodeGenerator.cs
@@ -0,0 +1,11 @@
+﻿using EzDbSchema.Core.Enums;
+
+namespace EzDbCodeGen.Core
+{
+    public class CodeGenerator : CodeGenBase, ITemplateRenderer
+    {
+        public CodeGenerator() : base()
+        {
+        }
+    }
+}
