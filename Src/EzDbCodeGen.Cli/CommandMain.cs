75eaf84e833b42335abbaf9092175b8f64c61277
Ricardo Vega <github@noctusoft.com>
2018-07-05 01:23:55 -0500

    Completed code generation tests for both multiple files and single files.

---
+++ Src/EzDbCodeGen.Cli/CommandMain.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using McMaster.Extensions.CommandLineUtils;
+using EzDbCodeGen.Internal;
+namespace EzDbCodeGen.Cli
+{
+    public static class CommandMain
+    {
+        public static void Enable(CommandLineApplication app) {
+            app.Name = "plex.codegen.cli";
+            app.Description = "Plex Code Generation Utility";
+            app.ExtendedHelpText = "This application will allow you to trigger code generation using the command line.  It will handle Plex and Unifier Conduit Conde Generations."
+                + Environment.NewLine + "This utility will also dump the schema into a handy JSON file.";
+
+            // Set the arguments to display the description and help text
+            app.HelpOption("-?|-h|--help");
+
+            app.OnExecute(() =>
+            {
+                app.ShowHint();
+                Environment.ExitCode = 0;
+                return Environment.ExitCode;
+            });
+        }
+    }
+
+    public static class CommandHelper<T>
+    {
+        public static IEnumerable<T> GetAll()
+        {
+            var assembly = Assembly.GetEntryAssembly();
+            var assemblies = assembly.GetReferencedAssemblies();
+
+            foreach (var assemblyName in assemblies)
+            {
+                assembly = Assembly.Load(assemblyName);
+
+                foreach (var ti in assembly.DefinedTypes)
+                {
+                    if (ti.ImplementedInterfaces.Contains(typeof(T)))
+                    {
+                        yield return (T)assembly.CreateInstance(ti.FullName);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Src/EzDbCodeGen.Tests/TemplateRenderTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using Xunit;
+using EzDbCodeGen.Core;
+using EzDbCodeGen.Internal;
+using EzDbCodeGen.Core.Config;
+using EzDbCodeGen.Core.Extentions.Strings;
+using EzDbCodeGen.Core.Compare;
+namespace EzDbCodeGen.Tests
+{
+    public class TemplateRenderTests 
+    {
+        string SchemaFileName = "";
+        public TemplateRenderTests()
+        {
+            this.SchemaFileName = (@"{ASSEMBLY_PATH}Resources" + Path.DirectorySeparatorChar + @"MySchemaName.db.json").ResolvePathVars();
+        }
+        [Fact]
+        public void RenderTemplateFileTest()
+        {
+            try
+            {
+                var codeGenerator = new CodeGenerator();
+                ITemplateInput template = new TemplateInputFileSource(SchemaFileName);
+                var database = template.LoadSchema().Filter();
+                var OutputPath = System.IO.Path.GetTempPath() + "MySchemaNameRender.txt";
+                if (File.Exists(OutputPath)) File.Delete(OutputPath);
+                codeGenerator.ProcessTemplate((@"{ASSEMBLY_PATH}Templates" + Path.DirectorySeparatorChar + @"SchemaRender.hbs").ResolvePathVars(), template, OutputPath);
+                Assert.True(File.Exists(codeGenerator.OutputPath), string.Format("Template Rendered Output file {0} was not created", codeGenerator.OutputPath));
+            }
+            catch (Exception ex)
+            {
+                Assert.True(false, ex.Message);
+            }
+            
+        }
+
+        [Fact]
+        public void RenderTemplateMultipleFilesTest()
+        {
+            try
+            {
+                var codeGenerator = new CodeGenerator();
+                ITemplateInput template = new TemplateInputFileSource(SchemaFileName);
+                var database = template.LoadSchema().Filter();
+                var OutputPath = System.IO.Path.GetTempPath() + "EzDbCodeGenTest" + Path.DirectorySeparatorChar;
+                if (Directory.Exists(OutputPath)) Directory.Delete(OutputPath, true);
+                codeGenerator.ProcessTemplate((@"{ASSEMBLY_PATH}Templates" + Path.DirectorySeparatorChar + @"SchemaRenderAsFiles.hbs").ResolvePathVars(), template, OutputPath);
+                Assert.True(Directory.Exists(codeGenerator.OutputPath), string.Format("Template Rendered Output files in path {0} was not created", codeGenerator.OutputPath));
+            }
+            catch (Exception ex)
+            {
+                Assert.True(false, ex.Message);
+            }
+
+        }
+
+    }
+}
