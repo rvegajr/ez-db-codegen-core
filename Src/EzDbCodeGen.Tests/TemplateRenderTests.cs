62ca8df30a1ae744df47ccdf882fbb3f151cab11
Ricardo Vega <github@noctusoft.com>
2018-07-13 11:22:23 -0500

    Added powershell script for code generation

--- Src/EzDbCodeGen.Tests/TemplateRenderTests.cs
+++ Src/EzDbCodeGen.Tests/TemplateRenderTests.cs
@@ -2,10 +2,7 @@
 using System.IO;
 using Xunit;
 using EzDbCodeGen.Core;
-using EzDbCodeGen.Internal;
-using EzDbCodeGen.Core.Config;
 using EzDbCodeGen.Core.Extentions.Strings;
-using EzDbCodeGen.Core.Compare;
 namespace EzDbCodeGen.Tests
 {
     public class TemplateRenderTests 
@@ -52,27 +49,41 @@ namespace EzDbCodeGen.Tests
             {
                 Assert.True(false, ex.Message);
             }
-
         }
 
         [Fact]
-        public void RenderTemplateDirectoryTest()
+        public void RenderTemplateSingleNoOutputPathTest()
         {
             try
             {
                 var codeGenerator = new CodeGenerator();
                 ITemplateInput template = new TemplateInputFileSource(SchemaFileName);
                 var database = template.LoadSchema().Filter();
-                var OutputPath = System.IO.Path.GetTempPath() + "EzDbCodeGenTest" + Path.DirectorySeparatorChar;
-                if (Directory.Exists(OutputPath)) Directory.Delete(OutputPath, true);
-                var rcs = codeGenerator.ProcessTemplate(("{ASSEMBLY_PATH}Templates").ResolvePathVars(), template, OutputPath);
+                codeGenerator.ProcessTemplate((@"{ASSEMBLY_PATH}Templates" + Path.DirectorySeparatorChar + @"SchemaRenderAsFiles.hbs").ResolvePathVars(), template);
                 Assert.True(Directory.Exists(codeGenerator.OutputPath), string.Format("Template Rendered Output files in path {0} was not created", codeGenerator.OutputPath));
             }
             catch (Exception ex)
             {
                 Assert.True(false, ex.Message);
             }
-
+        }
+        [Fact]
+        public void RenderTemplateNoOutputTest()
+        {
+            try
+            {
+                var codeGenerator = new CodeGenerator();
+                ITemplateInput template = new TemplateInputFileSource(SchemaFileName);
+                var database = template.LoadSchema().Filter();
+                var OutputPath = System.IO.Path.GetTempPath() + Path.DirectorySeparatorChar;
+                if (Directory.Exists(OutputPath)) Directory.Delete(OutputPath, true);
+                var rcs = codeGenerator.ProcessTemplate((@"{ASSEMBLY_PATH}Templates" + Path.DirectorySeparatorChar + @"SchemaRenderAsFilesNoOutput.hbs").ResolvePathVars(), template, OutputPath);
+                Assert.True(Directory.Exists(codeGenerator.OutputPath), string.Format("Template Rendered Output files in path {0} was not created", codeGenerator.OutputPath));
+            }
+            catch (Exception ex)
+            {
+                Assert.True(false, ex.Message);
+            }
         }
 
     }
