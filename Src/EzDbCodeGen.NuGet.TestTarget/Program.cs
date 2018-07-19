27f49cd10434a238aa18ed33b85c9efa4a0f63c8
Ricardo Vega <github@noctusoft.com>
2018-07-18 22:56:44 -0500

    completed asset dropping code and supporting published CLI code for code generation!

---
+++ Src/EzDbCodeGen.NuGet.TestTarget/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EzDbCodeGen.NuGet.TestTarget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class EzdbCodeGenerator : Core.CodeGenerator
+    {
+
+    }
+}
