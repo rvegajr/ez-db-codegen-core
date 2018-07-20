acd9ed03c507244daae553600f4899af7204f915
Ricardo Vega <github@noctusoft.com>
2018-07-19 23:29:44 -0500

    Updated readme and prep for nuget deployment

--- Src/VersionInfo.cs
+++ Src/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.65.*")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.65.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.0.*")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.1.0.0")]
