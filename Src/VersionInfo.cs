94526000bf9c283ced9830b3342977aea644c92c
Ricardo Vega <github@noctusoft.com>
2018-08-01 09:44:48 -0500

    Release Version 1.1.5 to nuget

--- Src/VersionInfo.cs
+++ Src/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.4.*")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.1.4.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.5.*")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.1.5.0")]
