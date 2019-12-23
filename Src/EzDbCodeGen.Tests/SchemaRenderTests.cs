b0591e450c5edcca1aca0838dca1a7549d8f925d
Ricky Vega <git@noctusoft.com>
2019-12-22 20:37:51 -0600

    don't know how it was originally working,   but fixed a bug where column annotation for fields that are the same name as their container object wasn't getting written out

--- Src/EzDbCodeGen.Tests/SchemaRenderTests.cs
+++ Src/EzDbCodeGen.Tests/SchemaRenderTests.cs
@@ -35,5 +35,28 @@ namespace EzDbCodeGen.Tests
             }
             
         }
+
+        [Fact]
+        public void MyConnectionTest()
+        {
+            try
+            {
+                var codeGenerator = new CodeGenerator();
+                ITemplateInput template = new TemplateInputDatabaseConnecton(@"Server=localhost;Database=MARS;user id=sa;password=sa");
+                Configuration.FromFile(@"C:\Dev\Summit\MARSApi\Src\MARSApi.Web\EzDbCodeGen\MARSApi.Web.config.json");
+                //ITemplateInput template = new TemplateInputDatabaseConnecton(@"Server=localhost;Database=WideWorldImportersDW;user id=User;password=Server@Database");
+                var database = template.LoadSchema().Filter();
+                var OutputPath = System.IO.Path.GetTempPath() + "MySchemaNameRender.txt";
+                if (File.Exists(OutputPath)) File.Delete(OutputPath);
+                codeGenerator.ConfigurationFileName = @"C:\Dev\Summit\MARSApi\Src\MARSApi.Web\EzDbCodeGen\MARSApi.Web.config.json";
+                codeGenerator.ProcessTemplate((@"C:\Dev\Summit\MARSApi\Src\MARSApi.Web\EzDbCodeGen\Templates\Ef6ModelsTemplateAnnotations.hbs").ResolvePathVars(), template, OutputPath);
+                Assert.True(File.Exists(codeGenerator.OutputPath), string.Format("Template Rendered Output file {0} was not created", codeGenerator.OutputPath));
+            }
+            catch (Exception ex)
+            {
+                Assert.True(false, ex.Message);
+            }
+
+        }
     }
 }
