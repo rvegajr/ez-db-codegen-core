db6a08f95b5d2ca77fb337fc605277cfdbd2c33b
Ricky Vega <git@noctusoft.com>
2019-12-26 16:30:48 -0600

    Deployment of nuget version 1.1.87

--- Src/VersionInfo.cs
+++ Src/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.85.*")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.1.85.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.87.*")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.1.87.0")]
