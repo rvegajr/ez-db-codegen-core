ce7e5a5cdb15bbd7d231c2562f0c4631e04ae554
Ricky Vega <git@noctusoft.com>
2020-06-02 21:22:05 -0500

    Added Nuke build and got a clean compile

---
+++ nuget/dotnet_tools_install.bat
@@ -0,0 +1,3 @@
+rem dotnet tool install --global --configfile disable_nuget.config --add-source ../artifacts EzDbCodeGen
+dotnet tool install --global --add-source ../artifacts EzDbCodeGen -v n
+pause
