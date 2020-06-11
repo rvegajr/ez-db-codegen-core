f8697bafb0a50991c3572cf476eb5ad6c3ced33d
Ricky Vega <git@noctusoft.com>
2020-06-10 23:53:52 -0500

    Added GitVersion to Server Build Script

--- build/Build.cs
+++ build/Build.cs
@@ -38,7 +38,7 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main () => Execute<Build>(x => x.Pack);
 
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
