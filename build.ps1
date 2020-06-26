2cb1380f73acb572d878c31d9a9571966b74f51d
Ricky Vega <git@noctusoft.com>
2020-06-25 22:16:52 -0500

    gitVer Help test v13 + nuget restore

--- build.ps1
+++ build.ps1
@@ -66,7 +66,7 @@ else {
 Write-Output "Microsoft (R) .NET Core SDK version $(& $env:DOTNET_EXE --version)"
 
 Write-Output "Restore Build.cs"
-ExecSafe { & $env:DOTNET_EXE restore $BuildProjectFile --verbosity quiet }
+ExecSafe { & $env:DOTNET_EXE restore $BuildProjectFile --verbosity quiet --disable-parallel}
 Write-Output "Build Build.cs"
 ExecSafe { & $env:DOTNET_EXE build $BuildProjectFile /nodeReuse:false -nologo -clp:NoSummary --verbosity quiet }
 ExecSafe { & $env:DOTNET_EXE run --project $BuildProjectFile --no-build -- $BuildArguments }
