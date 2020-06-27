7177f4bcc05ffccb49be5f822dccd6654f34bd1e
Ricky Vega <git@noctusoft.com>
2020-06-27 18:15:53 -0500

    gitVer Help test v13 + nuget restore V26

--- GitVersionUpdate.ps1
+++ GitVersionUpdate.ps1
@@ -1,1 +1,2 @@
-(Get-Content GitVersion.yml) | Foreach-Object {$_ -replace 'next-version:.+','next-version: X.X.99'} | Set-Content GitVersion.yml
+(Get-Content GitVersion.yml) | Foreach-Object {$_ -replace 'next-version:.+','next-version: X.X.77'} | Set-Content GitVersion.yml
+Write-Output "TEST" 
