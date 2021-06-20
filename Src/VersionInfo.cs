ad7f7874aa4783bc31eb80f33248478d949bba55
Ricky Vega <rvegajr@noctusoft.com>
2021-06-19 23:23:41 -0500

    Updated ReadMe and released and deployed Nuget V 5.0.10

--- Src/VersionInfo.cs
+++ Src/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: System.Reflection.AssemblyFileVersionAttribute("5.0.9.*")]
-[assembly: System.Reflection.AssemblyVersionAttribute("5.0.9.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("5.0.10.*")]
+[assembly: System.Reflection.AssemblyVersionAttribute("5.0.10.0")]
